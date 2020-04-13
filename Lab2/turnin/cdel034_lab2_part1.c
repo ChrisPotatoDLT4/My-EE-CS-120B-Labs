88ad4250b75739f89ddd8720180e0030ec5c9e56
Christopher De La Torre <47547565+ChrisPotatoDLT4@users.noreply.github.com>
2020-04-12 20:48:57 -0700

    Update cdel034_lab2_part1.c

--- Lab2/turnin/cdel034_lab2_part1.c
+++ Lab2/turnin/cdel034_lab2_part1.c
@@ -14,29 +14,27 @@
 
 int main(void) {
 
-        DDRA - 0x00;    //Configure port A's 8 pins to be inputs
+
+         DDRA = 0x00;    //Configure port A's 8 pins to be inputs
         PORTA = 0xFF;   //Haveing this to be output pins
 
         DDRB = 0xFF;    //configure port B's 8 pins t be outputs
         PORTB = 0x00;
 
-        unsigned char doorSensor = 0x00;
-        unsigned char lightSensor= 0x00;
+        unsigned char doorSensor = 0x00; //PA0
+        unsigned char lightSensor= 0x00; //PA1
 
-                doorSensor = PINA & 0x00; //Pin A bit 0 will have doorSensor
-                lightSensor = PINA & 0x01; //Pin A bit 1 will have lightSensor
 
         while(1){
+                 doorSensor = PINA & 0x01; //Pin A bit 0 will have doorSensor
+                lightSensor = PINA & 0x02; //Pin A bit 1 will have lightSensor
 
-                if( ((lightSensor == 0) && (doorSensor == 1)) )
+                if( doorSensor && (!lightSensor) )
                 {
                         PORTB = 0x01; //PORTB will be outputing the value (writing)
                 }                        //PORTB will output a number either 1 or 0
-                else { PORTB =0x00; }
-
+                else {PORTB = 0x00;}
         }
-
 
     return 1;
 }
-
